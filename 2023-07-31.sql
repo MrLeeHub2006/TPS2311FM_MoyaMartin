09ff35458b66e13706179de5010d93fd0dbe212c
MrLeeHub2006 <misterlee272006@gmail.com>
2023-08-02 07:20:06 -0500

    ejercicio corregido

--- 2023-07-31.sql
+++ 2023-07-31.sql
@@ -1,48 +1,61 @@
-CREATE SCHEMA `colegio`  DEFAULT CHARACTER SET utf8 ;
-USE   `colegio` ;
-CREATE TABLE `colegio`.`cursos`(
-`id`  INT NOT NULL AUTO_INCREMENT,
-`name` varchar (255) NULL,
-`n_reviews` INT NULL,
-`teacher_id` INT ,
-PRIMARY KEY (`id`));
+CREATE SCHEMA `actividad` DEFAULT CHARACTER SET utf8mb4 ;
+USE `actividad`;
+CREATE TABLE `actividad`.`courses` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NULL,
+  `n_reviews` INT NOT NULL ,
+  `teacher_id` INT NULL ,
+  PRIMARY KEY (`id`));
+INSERT INTO actividad.courses (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (100,'Fundamentos de SQL y Bases de Datos','1400','1');
+INSERT INTO actividad.courses (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (200,'Curso de MySQL y MariaDB','400','25');
+INSERT INTO actividad.courses (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (260,'Curso de Data Science','320','8');
+INSERT INTO actividad.courses (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (350,'Curso de Firebase','150','1');
+INSERT INTO actividad.courses (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (749,'Curso de Python','2300','8');
+INSERT INTO actividad.courses (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (750,'Curso de React.js','1500','16');
+CREATE TABLE `actividad`.`teacher` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
+INSERT INTO actividad.teacher (`id`,`name`) VALUES (1,'Israel');
+INSERT INTO actividad.teacher (`id`,`name`) VALUES (8,'Silvia');
+INSERT INTO actividad.teacher (`id`,`name`) VALUES (16,'Leomaris');
+INSERT INTO actividad.teacher (`id`,`name`) VALUES (25,'Juan');
 
-CREATE TABLE `colegio`.`teacher`(
-`id`  INT NOT NULL AUTO_INCREMENT,
-`names` varchar (255) NULL, 
-PRIMARY KEY (`id`));
+SELECT *
+FROM courses;
+
+SELECT *
+FROM teacher;
+
+SELECT teacher.name AS teacher_name,
+       courses.name AS course_name,
+       courses.n_reviews AS course_reviews
+FROM curses
+JOIN teacher ON curses.teacher_id = teacher.id
+ORDER BY teacher.name ASC, courses.n_reviews DESC;
+
+SELECT teacher.id AS teacher_id,
+       teacher.name AS teacher_name,
+       SUM(courses.n_reviews) AS total_reviews
+FROM teacher
+LEFT JOIN courses ON teacher.id = courses.teacher_id
+GROUP BY teacher.id, teacher.name;
+
+SELECT teacher.name AS teacher,
+       SUM(courses.n_reviews) AS total_reviews
+FROM teacher
+LEFT JOIN courses ON teacher.id = courses.teacher_id
+GROUP BY teacher.name;
+
+SELECT teacher.name AS teacher,
+       SUM(courses.n_reviews) AS total_reviews
+FROM teacher
+LEFT JOIN courses ON teacher.id = courses.teacher_id
+GROUP BY teacher.name
+ORDER BY total_reviews DESC;
 
 
 
-INSERT INTO `colegio`.`cursos` (`id`,`name`,`n_reviews`,`teacher_id`) VALUES (100,'Fundamentos de SQL y Base de datos','1400','1');
-INSERT INTO `colegio`.`cursos` (`id`,`name`,`n_reviews`,`teacher_id`) VALUES ('200','Curso de MySQL y Maria DB','400','25');
-INSERT INTO `colegio`.`cursos` (`id`,`name`,`n_reviews`,`teacher_id`) VALUES ('260','Curso de Data Science','320','8');
-INSERT INTO `colegio`.`cursos` (`id`,`name`,`n_reviews`,`teacher_id`) VALUES ('350','Curso de Firebase','150','1');
-INSERT INTO `colegio`.`cursos` (`id`,`name`,`n_reviews`,`teacher_id`) VALUES ('749','Curso de Python','2300','8');
-INSERT INTO `colegio`.`cursos` (`id`,`name`,`n_reviews`,`teacher_id`) VALUES ('750','Curso de React.js','1500','16');
-
-INSERT INTO `colegio`.`teacher` (`id`,`names`) VALUES (1,'Israel');
-INSERT INTO `colegio`.`teacher` (`id`,`names`) VALUES ('8','Silvia');
-INSERT INTO `colegio`.`teacher` (`id`,`names`) VALUES ('16','Leomaris');
-INSERT INTO `colegio`.`teacher` (`id`,`names`) VALUES ('25','Juan');
 
 
-SELECT  teacher_id, COUNT(teacher_id) AS Cursos_profe
-FROM cursos
-GROUP BY teacher_id;
-
-SELECT  teacher_id, COUNT(teacher_id) AS cursos_profe, SUM(n_reviews) total_reviews
-FROM cursos
-GROUP BY teacher_id;
-
-SELECT teacher;
-
-
-SELECT `names` AS teacher, SUM(n_reviews) total_reviews
-FROM  cursos, teacher
-GROUP BY `names`;
-
-SELECT  `names`,teacher_id, COUNT(teacher_id) AS cursos_profe, SUM(n_reviews) total_reviews
-FROM cursos, teacher
-GROUP BY teacher_id;
 
