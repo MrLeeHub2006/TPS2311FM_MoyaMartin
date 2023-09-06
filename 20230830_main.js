61276f2fe9f6fc8ba0574e769b23b77db0813118
MrLeeHub2006 <misterlee272006@gmail.com>
2023-09-06 11:45:29 -0500

    Visualización y concatenacion.

--- 20230830_main.js
+++ 20230830_main.js
@@ -5,8 +5,10 @@ const h1=document.querySelector('h1');
 const input1 =document.querySelector('#Calculo1');
 const input2 =document.querySelector('#Calculo2');
 const btn =document.querySelector('#btnCalcular');
+const pResult =document.querySelector('#result');
 
 function btnOnClick(){
-console.log(input1.value + input2.value);
-parseInt(input1.value +  )
+
+const sumaInputs = input1.value + input2.value;
+pResult.innerText = "Resultado: " + sumaInputs;
 }
