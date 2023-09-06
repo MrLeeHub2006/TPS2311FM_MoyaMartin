09d0e701dc82d0dbba1133609096edb7f43452b6
MrLeeHub2006 <misterlee272006@gmail.com>
2023-09-06 11:57:13 -0500

    addeverlistener

--- 20230830_main.js
+++ 20230830_main.js
@@ -4,8 +4,10 @@
 const h1=document.querySelector('h1');
 const input1 =document.querySelector('#Calculo1');
 const input2 =document.querySelector('#Calculo2');
-const btn =document.querySelector('#btnCalcular');
+const btn =document.querySelector('#    ');
 const pResult =document.querySelector('#result');
+
+btn.addEventListener('click', btnOnClick);
 
 function btnOnClick(){
 
