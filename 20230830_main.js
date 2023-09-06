ce4db9d2edc5abc8b7ff58a51b5b62f34f52f0a6
MrLeeHub2006 <misterlee272006@gmail.com>
2023-09-06 10:57:15 -0500

    eventos javascritp, interactuando con usuarios

--- 20230830_main.js
+++ 20230830_main.js
@@ -2,11 +2,11 @@
 
 
 const h1=document.querySelector('h1');
-const input1 =document.querySelector('Calculo1');
-const input2 =document.querySelector('Calculo2');
+const input1 =document.querySelector('#Calculo1');
+const input2 =document.querySelector('#Calculo2');
 const btn =document.querySelector('#btnCalcular');
 
 function btnOnClick(){
-console.log('Escuchando el evento de click')
+console.log(input1.value + input2.value);
 
 }
