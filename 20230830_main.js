2b93995ec0018540fb32266729b23d32f6fa9976
MrLeeHub2006 <misterlee272006@gmail.com>
2023-09-06 10:22:25 -0500

    onclick y cambios enel imput

--- 20230830_main.js
+++ 20230830_main.js
@@ -2,28 +2,7 @@
 
 
 const h1=document.querySelector('h1');
-const p=document.querySelector('p');
-const parrafito=document.getElementsByClassName('.parrafito');
-const pid=document.getElementById('pid');
-const input=document.querySelector('input');
-console.log(input.value);
-console.log({h1,p,parrafito,pid,input,});
+const input1 =document.querySelector('Calculo1');
+const input2 =document.querySelector('Calculo2');
+const btn =document.querySelector('#btnCalcular');
 
-
-h1.innerHTML= 'patito <br> feo'
-h1.innerText= 'patito <br> feo'
-// console.log (h1.getAttribute('class'));
-// h1.setAttribute ('class','roja');
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-// h1.classList.toggle('verde');
-// h1.classList.contains('verde');
-
-input.value='456'
-
-const img =document.createElement('img');
-img.setAttribute('src','https://th.bing.com/th/id/OIP.TtuHBTYgrAAnVyWAFukCegHaEK?w=323&h=181&c=7&r=0&o=5&pid=1.7');
-console.log(img);
-
-pid.appendChild(img);
