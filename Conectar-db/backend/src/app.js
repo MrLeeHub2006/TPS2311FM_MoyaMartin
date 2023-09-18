80c755e4a52bf5979e51a629dfc086a8333e0e3d
MrLeeHub2006 <misterlee272006@gmail.com>
2023-09-18 12:03:10 -0500

    peticion del post

--- Conectar-db/backend/src/app.js
+++ Conectar-db/backend/src/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res)=>{
+    res.send('Peticion GET');
+});
 
+app.post('/', (req, res)=>{
+    res.send('Peticion POST');
+})
 
 app.listen(3000, ()=>{
     console.log('servidor encendido');
