e76a10b9fec68b0d515e82c2fd37c61332aeec13
MrLeeHub2006 <misterlee272006@gmail.com>
2023-09-20 07:48:36 -0500

    explicando destructuring JS

--- Conectar-db/backend/src/app.js
+++ Conectar-db/backend/src/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const juan ={
+    apodo: 'juanito',
+    edad: 29,
+};
+
+
+//const apodo = juan.apodo;
+//const edad = juan.edad;
+
+
+const {apodo, edad}  = juan;
+
 app.get('/:id', (req, res)=>{
     const{ id } =req.params
     res.send('Peticion GET');
